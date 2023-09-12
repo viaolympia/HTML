4c6de31b49fd4e77bf679e8356d4874c06c82868
m3rcy.js <zarik.xz@gmail.com>
2023-09-12 17:36:18 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Отримуємо всі посилання для навігації та бокси
+    
     const links = document.querySelectorAll('a');
     const boxes = document.querySelectorAll('.box');
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
-            event.preventDefault(); // Зупиняємо перехід за посиланням
+            event.preventDefault();
 
-            // Видаляємо клас підсвічування для всіх боксів
+           
             boxes.forEach((box) => {
                 box.classList.remove('highlighted');
             });
 
-            // Отримуємо ідентифікатор з атрибута "data-target" посилання і додаємо клас підсвічування до відповідного боксу
+           
             const targetId = link.getAttribute('data-target');
             const targetBox = document.getElementById(targetId);
             if (targetBox) {
@@ -23,21 +23,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
     $(document).ready(function () {
         $('.gallery-section').slick({
-            slidesToShow: 3, // Кількість відображуваних слайдів
-            slidesToScroll: 1, // Кількість прокручуваних слайдів за один раз
-            autoplay: true, // Включити автоматичну прокрутку
-            autoplaySpeed: 3000, // Швидкість автоматичної прокрутки (у мілісекундах)
-            arrows: false, // Вимкнути кнопки "Попередній" і "Наступний"
+            slidesToShow: 3, 
+            slidesToScroll: 1, 
+            autoplay: true, 
+            autoplaySpeed: 3000, 
+            arrows: false, 
         });
     });
     $(document).ready(function () {
-        // Обробник події натискання кнопки "Gallery"
+      
         $('a[href="#gallery-section"]').click(function () {
-            // Прокручуємо сторінку до елемента з ID "gallery-container"
+            
             $('html, body').animate({
                 scrollTop: $('#gallery-section').offset().top
-            }, 1000); // 1000 мілісекунд (1 секунда) - час анімації прокрутки
-            return false; // Відміняємо дійсну адресу посилання
+            }, 1000); 
+            return false; 
         });
     });
 
